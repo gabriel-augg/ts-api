2b6c92418037c0e5d68b4a051324391e7f0f3745
Gabriel <gabrielgapseu@gmail.com>
2024-05-19 10:32:17 -0300

    Feat: add api-erros helper

--- src/helpers/api-erros.ts
+++ src/helpers/api-erros.ts
@@ -6,3 +6,27 @@ export class ApiError extends Error {
         this.statusCode = statusCode;
     }
 }
+
+export class BadRequestError extends ApiError {
+    constructor(message: string) {
+        super(message, 400);
+    }
+}
+
+export class UnauthorizedError extends ApiError {
+    constructor(message: string) {
+        super(message, 401);
+    }
+}
+
+export class NotFoundError extends ApiError {
+    constructor(message: string) {
+        super(message, 404);
+    }
+}
+
+export class UnprocessableEntityError extends ApiError {
+    constructor(message: string) {
+        super(message, 422);
+    }
+}
